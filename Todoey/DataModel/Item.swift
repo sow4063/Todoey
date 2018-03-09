a6373b179eaa397e73ede1dbe069d4c81c3cada4
sow4063 <sow4063@gmail.com>
2018-03-10 07:43:04 +0900

    Convert to MVC Design Pattern

---
+++ Todoey/DataModel/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by 이종익 on 2018. 3. 8..
+//  Copyright © 2018년 sow4063. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
